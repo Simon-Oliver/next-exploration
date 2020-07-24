bb5f105953934d3f22e97aecf02b000ab4a4f7b5
Simon-Oliver <stauffer.simon@outlook.com>
2020-07-24 18:58:33 +0200

    multiple lines

--- pages/index.js
+++ pages/index.js
@@ -13,31 +13,39 @@ import {
 } from 'recharts';
 
 const data = [
-  { name: 'Page A', uv: 4000, pv: 9000 },
-  { name: 'Page B', uv: 3000, pv: 7222 },
-  { name: 'Page C', uv: 2000, pv: 6222 },
-  { name: 'Page D', uv: 1223, pv: 5400 },
-  { name: 'Page E', uv: 1890, pv: 3200 },
-  { name: 'Page F', uv: 2390, pv: 2500 },
-  { name: 'Page G', uv: 3490, pv: 1209 },
+  { name: '15:00', temp: 25, hum: 55 },
+  { name: '16:00', temp: 27, hum: 60 },
+  { name: '17:00', temp: 26, hum: 58 },
+  { name: '18:00', temp: 28, hum: 70 },
+  { name: '19:00', temp: 30, hum: 78 },
+  { name: '20:00', temp: 22, hum: 48 },
+  { name: '21:00', temp: 25, hum: 50 },
 ];
 const SimpleAreaChart = () => {
   return (
-    <div style={{ height: '60vh', width: '80vw' }}>
-      <ResponsiveContainer width={700} height="80%">
-        <LineChart
-          width={800}
-          height={400}
-          data={data}
-          syncId="anyId"
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
+    <div style={{ height: '60vh', width: '80vw', background: 'red' }}>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart width={800} height={400} data={data} syncId="anyId" margin={{ top: 10 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" style={{ fill: 'white' }} dy={10} />
-          <YAxis style={{ fill: 'white' }} dx={0} />
+          <XAxis
+            tick={{ fill: '#000000' }}
+            stroke="#efefef"
+            dataKey="name"
+            style={{ fill: 'white' }}
+            dy={10}
+            height={40}
+          />
+          <YAxis
+            tick={{ fill: '#000000' }}
+            stroke="#efefef"
+            style={{ fill: 'white' }}
+            dx={0}
+            domain={[0, 100]}
+          />
           <Tooltip />
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-          <Brush margin={{ top: 10, right: 30, left: 0, bottom: 0 }} />
+          <Line type="monotone" dataKey="temp" stroke="#8884d8" fill="#8884d8" />
+          <Line type="monotone" dataKey="hum" stroke="#82ca9d" />
+          <Brush dataKey="name" margin={{ top: 10 }} />
         </LineChart>
       </ResponsiveContainer>
     </div>
