1e36eb60bf5c79f366beba58c5d6bcf0218fb95e
Simon-Oliver <stauffer.simon@outlook.com>
2020-07-28 20:17:56 +0200

    added dropdown menu

--- pages/bootstrap.js
+++ pages/bootstrap.js
@@ -1,22 +1,68 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import Card from "react-bootstrap/Card";
+import { Card, Dropdown, FormControl, Form } from 'react-bootstrap';
 
-function Bootstrap() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  <a
+    href=""
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+  </a>
+));
+
+// forwardRef again here!
+// Dropdown needs access to the DOM of the Menu to measure it
+const CustomMenu = React.forwardRef(
+  ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+    const [value, setValue] = useState('');
+
+    return (
+      <div ref={ref} style={style} className={className} aria-labelledby={labeledBy}>
+        <FormControl
+          autoFocus
+          className="mx-3 my-2 w-auto"
+          type="date"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+        />
+
+        <ul className="list-unstyled">
+          {React.Children.toArray(children).filter(
+            (child) => !value || child.props.children.toLowerCase().startsWith(value)
+          )}
+        </ul>
+      </div>
+    );
+  }
+);
+
+const Bootstrap = () => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <Dropdown>
+      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+        <FontAwesomeIcon icon={faChevronDown} size="1x" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu as={CustomMenu}>
+        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+        <Dropdown.Item eventKey="3" active>
+          Orange
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
-}
+};
 
 export default Bootstrap;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import React from "react";
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
 import {
   LineChart,
   Line,
@@ -11,46 +11,49 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
-} from "recharts";
+} from 'recharts';
 
-import style from "./lineChart.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import style from './lineChart.module.css';
 
 const data = [
-  { name: "00:00", temp: 42, hum: 55 },
-  { name: "01:00", temp: 12, hum: 20 },
-  { name: "02:00", temp: 14, hum: 19 },
-  { name: "03:00", temp: 16, hum: 34 },
-  { name: "04:00", temp: 18, hum: 26 },
-  { name: "05:00", temp: 27, hum: 37 },
-  { name: "06:00", temp: 34, hum: 19 },
-  { name: "07:00", temp: 25, hum: 43 },
-  { name: "08:00", temp: 29, hum: 63 },
-  { name: "09:00", temp: 25, hum: 75 },
-  { name: "10:00", temp: 29, hum: 84 },
-  { name: "11:00", temp: 25, hum: 43 },
-  { name: "12:00", temp: 19, hum: 32 },
-  { name: "13:00", temp: 21, hum: 37 },
-  { name: "14:00", temp: 22, hum: 40 },
-  { name: "15:00", temp: 25, hum: 55 },
-  { name: "16:00", temp: 27, hum: 60 },
-  { name: "17:00", temp: 26, hum: 58 },
-  { name: "18:00", temp: 28, hum: 70 },
-  { name: "19:00", temp: 30, hum: 78 },
-  { name: "20:00", temp: 22, hum: 48 },
-  { name: "21:00", temp: 25, hum: 50 },
-  { name: "22:00", temp: 30, hum: 66 },
-  { name: "23:00", temp: 46, hum: 70 },
-  { name: "24:00", temp: 70, hum: 70 },
+  { name: '00:00', temp: 42, hum: 55 },
+  { name: '01:00', temp: 12, hum: 20 },
+  { name: '02:00', temp: 14, hum: 19 },
+  { name: '03:00', temp: 16, hum: 34 },
+  { name: '04:00', temp: 18, hum: 26 },
+  { name: '05:00', temp: 27, hum: 37 },
+  { name: '06:00', temp: 34, hum: 19 },
+  { name: '07:00', temp: 25, hum: 43 },
+  { name: '08:00', temp: 29, hum: 63 },
+  { name: '09:00', temp: 25, hum: 75 },
+  { name: '10:00', temp: 29, hum: 84 },
+  { name: '11:00', temp: 25, hum: 43 },
+  { name: '12:00', temp: 19, hum: 32 },
+  { name: '13:00', temp: 21, hum: 37 },
+  { name: '14:00', temp: 22, hum: 40 },
+  { name: '15:00', temp: 25, hum: 55 },
+  { name: '16:00', temp: 27, hum: 60 },
+  { name: '17:00', temp: 26, hum: 58 },
+  { name: '18:00', temp: 28, hum: 70 },
+  { name: '19:00', temp: 30, hum: 78 },
+  { name: '20:00', temp: 22, hum: 48 },
+  { name: '21:00', temp: 25, hum: 50 },
+  { name: '22:00', temp: 30, hum: 66 },
+  { name: '23:00', temp: 46, hum: 70 },
+  { name: '24:00', temp: 70, hum: 70 },
 ];
+
 const Graph = () => {
   return (
     <div className={style.lineChartContainer}>
-      <div className={style.line}>
-        <div className={style.heading}>
-          <h5>Temperatur & Humidity</h5>
-        </div>
+      <div className={style.heading}>
+        <h5>Temperatur & Humidity</h5>
+        <FontAwesomeIcon icon={faChevronDown} size="1x" />
       </div>
-      <ResponsiveContainer width="90%" height="90%">
+      <ResponsiveContainer className={style.line} width="90%" height="90%">
         <LineChart
           data={data}
           syncId="anyId"
@@ -63,27 +66,22 @@ const Graph = () => {
         >
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis
-            tick={{ fill: "#000000", fontSize: "0.5rem" }}
+            tick={{ fill: '#000000', fontSize: '0.5rem' }}
             stroke="#efefef"
             dataKey="name"
-            style={{ fill: "white" }}
+            style={{ fill: 'white' }}
             dy={10}
             height={40}
           />
           <YAxis
-            tick={{ fill: "#000000", fontSize: "0.7rem" }}
+            tick={{ fill: '#000000', fontSize: '0.7rem' }}
             stroke="#efefef"
-            style={{ fill: "white" }}
+            style={{ fill: 'white' }}
             dx={0}
             domain={[0, 100]}
           />
           <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="temp"
-            stroke="#8884d8"
-            fill="#8884d8"
-          />
+          <Line type="monotone" dataKey="temp" stroke="#8884d8" fill="#8884d8" />
           <Line type="monotone" dataKey="hum" stroke="#82ca9d" />
         </LineChart>
       </ResponsiveContainer>
