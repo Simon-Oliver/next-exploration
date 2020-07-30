04ff4ed331483a35bfc72cef17934bb21729fecd
Simon-Oliver <stauffer.simon@outlook.com>
2020-07-30 21:24:15 +0200

    added date and time selection

--- pages/bootstrap.js
+++ pages/bootstrap.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 
-import { Card, Dropdown, FormControl, Form } from 'react-bootstrap';
+import { Card, Dropdown, FormControl, Form, Row, Col } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
+import style from './test.module.css';
+import moment from 'moment';
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
@@ -24,23 +26,64 @@ const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
 // Dropdown needs access to the DOM of the Menu to measure it
 const CustomMenu = React.forwardRef(
   ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState({
+      startDate: moment().format('YYYY-MM-DD'),
+      endDate: moment().add(7, 'days').format('YYYY-MM-DD'),
+      startTime: '00:00',
+      endTime: '24:00',
+    });
+
+    const timeItem = () => {
+      const arr = [];
+      for (let index = 0; index < 25; index++) {
+        if (index === 0) {
+          arr.push(<option key={index}>00:00</option>);
+        } else if (index < 10) {
+          arr.push(<option key={index}>{`0${index}:00`}</option>);
+        } else {
+          arr.push(<option key={index}>{`${index}:00`}</option>);
+        }
+      }
+      return arr;
+    };
+
+    const handleChange = (evt) => {
+      const value = evt.target.value;
+      console.log(value);
+      setValue({
+        ...value,
+        [evt.target.id]: value,
+      });
+    };
 
     return (
       <div ref={ref} style={style} className={className} aria-labelledby={labeledBy}>
-        <FormControl
-          autoFocus
-          className="mx-3 my-2 w-auto"
-          type="date"
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-        />
-
-        <ul className="list-unstyled">
-          {React.Children.toArray(children).filter(
-            (child) => !value || child.props.children.toLowerCase().startsWith(value)
-          )}
-        </ul>
+        <Form onChange={(e) => handleChange(e)}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="startDate">
+              <Form.Label>Start Date</Form.Label>
+              <FormControl autoFocus type="date" defaultValue={value.startDate} />
+            </Form.Group>
+            <Form.Group as={Col} controlId="startTime">
+              <Form.Label>Time</Form.Label>
+              <Form.Control as="select" defaultValue={value.startTime}>
+                {timeItem()}
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} controlId="endDate">
+              <Form.Label>End Date</Form.Label>
+              <FormControl autoFocus type="date" defaultValue={value.endDate} />
+            </Form.Group>
+            <Form.Group as={Col} controlId="endTime">
+              <Form.Label>Time</Form.Label>
+              <Form.Control as="select" defaultValue={value.endTime}>
+                {timeItem()}
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
+        </Form>
       </div>
     );
   }
@@ -53,14 +96,7 @@ const Bootstrap = () => {
         <FontAwesomeIcon icon={faChevronDown} size="1x" />
       </Dropdown.Toggle>
 
-      <Dropdown.Menu as={CustomMenu}>
-        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-        <Dropdown.Item eventKey="3" active>
-          Orange
-        </Dropdown.Item>
-        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-      </Dropdown.Menu>
+      <Dropdown.Menu className={style.dropdown} as={CustomMenu}></Dropdown.Menu>
     </Dropdown>
   );
 };
