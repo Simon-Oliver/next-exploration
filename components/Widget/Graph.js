edf8a8a0758a373191314e041a2c551ca6c12d77
Simon-Oliver <stauffer.simon@outlook.com>
2020-08-06 22:25:06 +0200

    added graph

--- components/Widget/Graph.js
+++ components/Widget/Graph.js
@@ -22,34 +22,6 @@ import ChartDropDown from './ChartDropDown';
 
 import style from './Graph.module.css';
 
-const data = [
-  { name: '00:00', temp: 42, hum: 55 },
-  { name: '01:00', temp: 12, hum: 20 },
-  { name: '02:00', temp: 14, hum: 19 },
-  { name: '03:00', temp: 16, hum: 34 },
-  { name: '04:00', temp: 18, hum: 26 },
-  { name: '05:00', temp: 27, hum: 37 },
-  { name: '06:00', temp: 34, hum: 19 },
-  { name: '07:00', temp: 25, hum: 43 },
-  { name: '08:00', temp: 29, hum: 63 },
-  { name: '09:00', temp: 25, hum: 75 },
-  { name: '10:00', temp: 29, hum: 84 },
-  { name: '11:00', temp: 25, hum: 43 },
-  { name: '12:00', temp: 19, hum: 32 },
-  { name: '13:00', temp: 21, hum: 37 },
-  { name: '14:00', temp: 22, hum: 40 },
-  { name: '15:00', temp: 25, hum: 55 },
-  { name: '16:00', temp: 27, hum: 60 },
-  { name: '17:00', temp: 26, hum: 58 },
-  { name: '18:00', temp: 28, hum: 70 },
-  { name: '19:00', temp: 30, hum: 78 },
-  { name: '20:00', temp: 22, hum: 48 },
-  { name: '21:00', temp: 25, hum: 50 },
-  { name: '22:00', temp: 30, hum: 66 },
-  { name: '23:00', temp: 46, hum: 70 },
-  { name: '24:00', temp: 70, hum: 70 },
-];
-
 const filterData = (startTime, endTime, data) => {
   const format = 'hh:mm';
   startTime = moment(startTime, format).subtract(1, 'hours');
@@ -61,17 +33,30 @@ const filterData = (startTime, endTime, data) => {
   return newData;
 };
 
-const Graph = () => {
+// const minMax = (data) => {
+//   const min = 0;
+//   const max = 85;
+//   for (const e in data) {
+//     if (e.temp < min) min = e.temp;
+//     if (e.temp > max) max = e.temp;
+//   }
+//   return [min, max];
+// };
+
+const Graph = (props) => {
   const { state, dispatch } = useStore();
 
+  const { data, title } = props;
+
   useEffect(() => {
+    console.log('Props ----->', props);
     console.log(state);
   }, [state]);
 
   return (
     <div className={style.lineChartContainer}>
       <div className={style.heading}>
-        <h5>Temperatur & Humidity</h5>
+        <h5>{title ? title : 'Temperature & Humidity'}</h5>
         <ChartDropDown></ChartDropDown>
       </div>
       <ResponsiveContainer className={style.line} width="90%" height="90%">
--- pages/tempprobe.js
+++ pages/tempprobe.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import Graph from '../components/Widget/Graph';
 
+const data = [
+  { name: '00:00', temp: 42 },
+  { name: '01:00', temp: 12 },
+  { name: '02:00', temp: 14 },
+  { name: '03:00', temp: 16 },
+  { name: '04:00', temp: 18 },
+  { name: '05:00', temp: 27 },
+  { name: '06:00', temp: 34 },
+  { name: '07:00', temp: 25 },
+  { name: '08:00', temp: 29 },
+  { name: '09:00', temp: 25 },
+  { name: '10:00', temp: 29 },
+  { name: '11:00', temp: 25 },
+  { name: '12:00', temp: 19 },
+  { name: '13:00', temp: 21 },
+  { name: '14:00', temp: 22 },
+  { name: '15:00', temp: 25 },
+  { name: '16:00', temp: 27 },
+  { name: '17:00', temp: 26 },
+  { name: '18:00', temp: 28 },
+  { name: '19:00', temp: 30 },
+  { name: '20:00', temp: 22 },
+  { name: '21:00', temp: 25 },
+  { name: '22:00', temp: 30 },
+  { name: '23:00', temp: 46 },
+  { name: '24:00', temp: 70 },
+];
+
 const TempProbe = () => {
   return (
-    <div>
-      <h2>Temp Probe</h2>
-    </div>
+    <>
+      <Graph data={data} title={'Probe Temperature'}></Graph>
+    </>
   );
 };
 
